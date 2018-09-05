bb81dd0d4fa6c5613c5642ca32d725191bedce35
YannHyaric <ylhalmeida@gmail.com>
2018-09-05 20:26:52 -0300

    cria tela aluno

---
+++ app/src/main/java/exemplo02/dcc196/ufjf/packagebr/exercicio/Aluno.java
@@ -0,0 +1,13 @@
+package exemplo02.dcc196.ufjf.packagebr.exercicio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Aluno extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_aluno);
+    }
+}
