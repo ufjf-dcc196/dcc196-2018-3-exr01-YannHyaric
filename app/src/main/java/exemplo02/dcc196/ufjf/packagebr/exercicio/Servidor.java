db7ab1339c7a1b40c4c96fcb5db59ed1a3ae984a
YannHyaric <ylhalmeida@gmail.com>
2018-09-05 20:35:13 -0300

    atualiza tela aluno e servidor

---
+++ app/src/main/java/exemplo02/dcc196/ufjf/packagebr/exercicio/Servidor.java
@@ -0,0 +1,13 @@
+package exemplo02.dcc196.ufjf.packagebr.exercicio;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Servidor extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_servidor);
+    }
+}
