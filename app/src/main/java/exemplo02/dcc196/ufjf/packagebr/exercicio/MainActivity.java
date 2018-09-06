997b626bab65576c985bbdf8561ba8ba2005f68f
YannHyaric <ylhalmeida@gmail.con>
2018-09-05 22:15:21 -0300

    atualizar tela MainActivity

--- app/src/main/java/exemplo02/dcc196/ufjf/packagebr/exercicio/MainActivity.java
+++ app/src/main/java/exemplo02/dcc196/ufjf/packagebr/exercicio/MainActivity.java
@@ -3,14 +3,17 @@ package exemplo02.dcc196.ufjf.packagebr.exercicio;
 import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.ArrayMap;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 public class MainActivity extends Activity {
     private Button btnAluno,btnServ,btnExt;
     private TextView numAlunos,numServ,numExt;
     private static int ALUNOS=0,SERVIDORES=0,EXTERNOS=0;
+    private ArrayMap<EditText,EditText> alunos,servidores,externos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,6 +25,7 @@ public class MainActivity extends Activity {
         numAlunos=findViewById(R.id.numAlunos);
         numServ=findViewById(R.id.numServ);
         numExt=findViewById(R.id.numExt);
+
         btnAluno.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
